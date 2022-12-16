66eab44ceba1860f9ecccd8738d50aefc7299110
rzkynovan <rizkynovan21@gmail.com>
2022-12-16 17:35:41 +0700

    inate

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".galery-container", {
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween:0,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
